0af36854d0f56ca24305306f3e29c18ae982132f
Anna Madejczyk <anna08@vp.pl>
2015-06-22 22:52:35 +0200

    Changes in language library.

---
+++ includes/languages/english/charge_wallet.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['choose_charge_method'] = "Wybierz sposób doładowania";
+$lang['charge_sms_option'] = "{1} {2} - otrzymujesz {3} {4}";
+$lang['charge_amount'] = "Kwota doładowania";
+
+$lang['wrong_charge_method'] = "Wybrano błędną metodę doładowania.";
+$lang['charge_amount_not_chosen'] = "Nie wybrano wysokości doładowania ( koszt SMSa ).";
+$lang['charge_amount_too_low'] = "Wartość doładowania musi być większa niż {1}";
