2dccd92bdf611213fedfc9d362fae741272b3e45
Anna Madejczyk <anna08@vp.pl>
2015-07-07 22:28:11 +0200

    Language modifications

--- includes/languages/global.php
+++ includes/languages/global.php
@@ -4,3 +4,7 @@ $l['languages'] = array(
 	'polish' => "Polski",
 	'english' => "English"
 );
+
+// $l['pass_change'] = "W celu zmiany hasła do swojego konta, podaj stare hasło. Następnie wpisz nowe hasło oraz wprowadź je ponownie."; , $l['pass_reset'] = "W celu zmiany hasła do swojego konta, podaj nowe hasło, a następnie wpisz je ponownie."//
+
+
