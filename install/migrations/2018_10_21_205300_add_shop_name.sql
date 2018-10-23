01e518a3df12dc7eb32c9940ba7660b65bea2468
Michał Budziak <michal.mariusz.b@gmail.com>
2018-10-23 18:33:36 +0200

    Make token from license password and login

--- install/migrations/2018_10_21_205300_add_shop_name.sql
+++ install/migrations/2018_10_21_205300_add_shop_name.sql
@@ -1,1 +1,10 @@
 INSERT INTO `ss_settings` (`key`, `value`) VALUES ('shop_name', '');
+UPDATE `ss_settings`
+SET `value` =(
+    CASE
+    WHEN (SELECT COUNT(*) FROM (SELECT * FROM `ss_settings`) as a WHERE `key` = 'license_login' AND `value` = 'license') > 0 THEN `value`
+    ELSE SHA1(CONCAT((SELECT `value` FROM (SELECT * FROM `ss_settings`) as b WHERE `key` = 'license_login'), '#', (SELECT `value` FROM (SELECT * FROM `ss_settings`) as c WHERE `key` = 'license_password')))
+    END
+)
+WHERE `key` = 'license_password';
+UPDATE `ss_settings` SET `value` = 'license' WHERE `key` = 'license_login';
