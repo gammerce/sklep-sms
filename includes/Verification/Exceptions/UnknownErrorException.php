2815c96e2656074fc5be6679e4b700a51e7167b0
Michał Budziak <michal.mariusz.b@gmail.com>
2018-12-23 13:19:00 +0100

    Translations

--- includes/Verification/Exceptions/UnknownErrorException.php
+++ includes/Verification/Exceptions/UnknownErrorException.php
@@ -8,6 +8,6 @@ class UnknownErrorException extends SmsPaymentException
 {
     public function __construct($message = "")
     {
-        parent::__construct($message, "unknown");
+        parent::__construct($message, "unknown_error");
     }
 }
