1239a9d6ca1a40e77964be0224abb6f92983c8c8
Michał Budziak <michal.mariusz.b@gmail.com>
2018-12-25 11:41:34 +0100

    SQL

---
+++ install/migrations/2018_12_25_205300_change_cssetti_numbers.sql
@@ -0,0 +1,14 @@
+DELETE FROM `ss_sms_numbers` WHERE `tariff` = 26 AND `service` = 'cssetti';
+DELETE FROM `ss_sms_numbers` WHERE `tariff` = 7 AND `service` = 'cssetti';
+DELETE FROM `ss_sms_numbers` WHERE `tariff` = 8 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '71480' WHERE `tariff` = 1 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '72480' WHERE `tariff` = 2 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '73480' WHERE `tariff` = 3 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '74480' WHERE `tariff` = 4 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '75480' WHERE `tariff` = 5 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '76480' WHERE `tariff` = 6 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '79480' WHERE `tariff` = 9 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '91400' WHERE `tariff` = 14 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '91900' WHERE `tariff` = 19 AND `service` = 'cssetti';
+UPDATE `ss_sms_numbers` SET `number` = '92521' WHERE `tariff` = 25 AND `service` = 'cssetti';
+INSERT IGNORE INTO `ss_sms_numbers` SET `number` = '92022', `tariff` = 20 AND `service` = 'cssetti';
