8879db64beba9aeb6b4ea5bb9dccf7cc6e9f1d8c
Michał Budziak <michal.mariusz.b@gmail.com>
2019-07-03 20:32:12 +0200

    Remove script name

--- install/update.php
+++ install/update.php
@@ -1,7 +1,6 @@
 <?php
 
 define('IN_SCRIPT', '1');
-define('SCRIPT_NAME', 'ajax_install_update');
 
 error_reporting(E_ERROR | E_CORE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR | E_COMPILE_ERROR);
 ini_set('display_errors', 1);
