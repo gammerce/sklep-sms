3b14d183337c72e3e7331b67e1472a1a0118462b
Michał Budziak <michal.mariusz.b@gmail.com>
2019-10-28 18:45:14 +0100

    Add email field

--- includes/Pages/PageProfile.php
+++ includes/Pages/PageProfile.php
@@ -21,11 +21,12 @@ class PageProfile extends Page implements IBeLoggedMust
     protected function content(array $query, array $body)
     {
         $user = $this->auth->user();
+        $email = htmlspecialchars($user->getEmail(false));
         $username = htmlspecialchars($user->getUsername(false));
         $forename = htmlspecialchars($user->getForename());
         $surname = htmlspecialchars($user->getSurname());
         $steamId = htmlspecialchars($user->getSteamId());
 
-        return $this->template->render("profile", compact("username", "forename", "surname", "steamId"));
+        return $this->template->render("profile", compact("email", "username", "forename", "surname", "steamId"));
     }
 }
