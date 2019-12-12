db705cf687e4f91f639fae4b26da59e205b1c8f9
Michał Budziak <michal.mariusz.b@gmail.com>
2019-12-12 14:05:55 +0100

    Default value

--- migrations/2019_10_28_13200_add_user_steamid.php
+++ migrations/2019_10_28_13200_add_user_steamid.php
@@ -6,7 +6,7 @@ class AddUserSteamId extends Migration
 {
     public function up()
     {
-        $queries = ["ALTER TABLE `ss_users` ADD steam_id VARCHAR (32) NOT NULL;"];
+        $queries = ["ALTER TABLE `ss_users` ADD steam_id VARCHAR (32) NOT NULL DEFAULT '';"];
         $this->executeQueries($queries);
     }
 }
