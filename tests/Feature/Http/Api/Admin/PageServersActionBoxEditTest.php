b76786995e66c5fcf6e314168f3f8c88fb880c0a
Michał Budziak <michal.mariusz.b@gmail.com>
2019-12-23 18:12:23 +0100

    Server edit

--- tests/Feature/Http/Api/Admin/PageServersActionBoxEditTest.php
+++ tests/Feature/Http/Api/Admin/PageServersActionBoxEditTest.php
@@ -3,17 +3,18 @@ namespace Tests\Feature\Http\Api\Admin;
 
 use Tests\Psr4\TestCases\HttpTestCase;
 
-class PageServersActionBoxAddTest extends HttpTestCase
+class PageServersActionBoxEditTest extends HttpTestCase
 {
     /** @test */
     public function get_add_box()
     {
         // give
+        $server = $this->factory->server();
         $admin = $this->factory->admin();
         $this->actAs($admin);
 
         // when
-        $response = $this->get("/api/admin/pages/servers/action_boxes/server_add");
+        $response = $this->get("/api/admin/pages/servers/action_boxes/server_edit", ["id" => $server->getId()]);
 
         // then
         $this->assertSame(200, $response->getStatusCode());
@@ -26,11 +27,12 @@ class PageServersActionBoxAddTest extends HttpTestCase
     public function requires_permission_to_get()
     {
         // give
+        $server = $this->factory->server();
         $admin = $this->factory->user();
         $this->actAs($admin);
 
         // when
-        $response = $this->get("/api/admin/pages/servers/action_boxes/server_add");
+        $response = $this->get("/api/admin/pages/servers/action_boxes/server_edit", ["id" => $server->getId()]);
 
         // then
         $this->assertSame(200, $response->getStatusCode());
