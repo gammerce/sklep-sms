406496b4f2c1e740f352b1f41751ad2a775412cc
Michał Budziak <michal.mariusz.b@gmail.com>
2020-01-09 22:12:54 +0100

    Remove mybb extra groups simple

--- includes/ServiceModules/MybbExtraGroups/ServiceMybbExtraGroups.php
+++ includes/ServiceModules/MybbExtraGroups/ServiceMybbExtraGroups.php
@@ -8,21 +8,40 @@ use App\Models\Purchase;
 use App\Models\Service;
 use App\Payment\AdminPaymentService;
 use App\Payment\BoughtServiceService;
+use App\ServiceModules\Interfaces\IServiceAdminManage;
+use App\ServiceModules\Interfaces\IServiceCreate;
 use App\ServiceModules\Interfaces\IServicePurchase;
 use App\ServiceModules\Interfaces\IServicePurchaseWeb;
 use App\ServiceModules\Interfaces\IServiceUserOwnServices;
 use App\ServiceModules\Interfaces\IServiceUserServiceAdminAdd;
+use App\ServiceModules\Interfaces\IServiceUserServiceAdminDisplay;
+use App\ServiceModules\ServiceModule;
 use App\System\Auth;
 use App\System\Database;
 use App\System\Heart;
+use App\System\Settings;
+use App\Translation\TranslationManager;
+use App\Translation\Translator;
+use App\View\CurrentPage;
+use App\View\Html\BodyRow;
+use App\View\Html\Cell;
+use App\View\Html\HeadCell;
+use App\View\Html\Structure;
+use App\View\Html\Wrapper;
 use PDOException;
 
-class ServiceMybbExtraGroups extends ServiceMybbExtraGroupsSimple implements
+class ServiceMybbExtraGroups extends ServiceModule implements
+    IServiceAdminManage,
+    IServiceCreate,
+    IServiceUserServiceAdminDisplay,
     IServicePurchase,
     IServicePurchaseWeb,
     IServiceUserServiceAdminAdd,
     IServiceUserOwnServices
 {
+    const MODULE_ID = "mybb_extra_groups";
+    const USER_SERVICE_TABLE = "user_service_mybb_extra_groups";
+
     /** @var array */
     private $groups = [];
 
@@ -39,6 +58,12 @@ class ServiceMybbExtraGroups extends ServiceMybbExtraGroupsSimple implements
 
     /** @var Heart */
     private $heart;
+
+    /** @var Translator */
+    private $lang;
+
+    /** @var Settings */
+    private $settings;
 
     /** @var BoughtServiceService */
     private $boughtServiceService;
@@ -58,6 +83,10 @@ class ServiceMybbExtraGroups extends ServiceMybbExtraGroupsSimple implements
         $this->boughtServiceService = $this->app->make(BoughtServiceService::class);
         $this->logger = $this->app->make(DatabaseLogger::class);
         $this->adminPaymentService = $this->app->make(AdminPaymentService::class);
+        $this->settings = $this->app->make(Settings::class);
+        /** @var TranslationManager $translationManager */
+        $translationManager = $this->app->make(TranslationManager::class);
+        $this->lang = $translationManager->user();
 
         $serviceData = $this->service ? $this->service->getData() : null;
         if (isset($serviceData['mybb_groups'])) {
@@ -67,6 +96,211 @@ class ServiceMybbExtraGroups extends ServiceMybbExtraGroupsSimple implements
         $this->dbUser = array_get($serviceData, 'db_user', '');
         $this->dbPassword = array_get($serviceData, 'db_password', '');
         $this->dbName = array_get($serviceData, 'db_name', '');
+    }
+
+    public function serviceAdminExtraFieldsGet()
+    {
+        // WEB
+        if ($this->showOnWeb()) {
+            $webSelYes = "selected";
+            $webSelNo = "";
+        } else {
+            $webSelYes = "";
+            $webSelNo = "selected";
+        }
+
+        // We're in the edit mode
+        if ($this->service !== null) {
+            // DB
+            $dbPassword = strlen(array_get($this->service->getData(), 'db_password'))
+                ? "********"
+                : "";
+            $dbHost = array_get($this->service->getData(), 'db_host');
+            $dbUser = array_get($this->service->getData(), 'db_user');
+            $dbName = array_get($this->service->getData(), 'db_name');
+
+            // MyBB groups
+            $mybbGroups = array_get($this->service->getData(), 'mybb_groups');
+        }
+
+        return $this->template->render(
+            "services/mybb_extra_groups/extra_fields",
+            compact(
+                'webSelNo',
+                'webSelYes',
+                'mybbGroups',
+                'dbHost',
+                'dbUser',
+                'dbPassword',
+                'dbName'
+            ) + ['moduleId' => $this->getModuleId()],
+            true,
+            false
+        );
+    }
+
+    public function serviceAdminManagePre(array $data)
+    {
+        $warnings = [];
+
+        // Web
+        if (!in_array($data['web'], ["1", "0"])) {
+            $warnings['web'][] = $this->lang->t('only_yes_no');
+        }
+
+        // MyBB groups
+        if (!strlen($data['mybb_groups'])) {
+            $warnings['mybb_groups'][] = $this->lang->t('field_no_empty');
+        } else {
+            $groups = explode(",", $data['mybb_groups']);
+            foreach ($groups as $group) {
+                if (!my_is_integer($group)) {
+                    $warnings['mybb_groups'][] = $this->lang->t('group_not_integer');
+                    break;
+                }
+            }
+        }
+
+        // Db host
+        if (!strlen($data['db_host'])) {
+            $warnings['db_host'][] = $this->lang->t('field_no_empty');
+        }
+
+        // Db user
+        if (!strlen($data['db_user'])) {
+            $warnings['db_user'][] = $this->lang->t('field_no_empty');
+        }
+
+        // Db password
+        if ($this->service === null && !strlen($data['db_password'])) {
+            $warnings['db_password'][] = $this->lang->t('field_no_empty');
+        }
+
+        // Db name
+        if (!strlen($data['db_name'])) {
+            $warnings['db_name'][] = $this->lang->t('field_no_empty');
+        }
+
+        return $warnings;
+    }
+
+    public function serviceAdminManagePost(array $data)
+    {
+        $mybbGroups = explode(",", $data['mybb_groups']);
+        foreach ($mybbGroups as $key => $group) {
+            $mybbGroups[$key] = trim($group);
+            if (!strlen($mybbGroups[$key])) {
+                unset($mybbGroups[$key]);
+            }
+        }
+
+        $extraData = [
+            'mybb_groups' => implode(",", $mybbGroups),
+            'web' => $data['web'],
+            'db_host' => $data['db_host'],
+            'db_user' => $data['db_user'],
+            'db_password' => array_get(
+                $data,
+                'db_password',
+                array_get($this->service->getData(), 'db_password')
+            ),
+            'db_name' => $data['db_name'],
+        ];
+
+        return [
+            'data' => $extraData,
+        ];
+    }
+
+    public function userServiceAdminDisplayTitleGet()
+    {
+        return $this->lang->t('mybb_groups');
+    }
+
+    public function userServiceAdminDisplayGet(array $query, array $body)
+    {
+        /** @var CurrentPage $currentPage */
+        $currentPage = $this->app->make(CurrentPage::class);
+
+        $pageNumber = $currentPage->getPageNumber();
+
+        $wrapper = new Wrapper();
+        $wrapper->setSearch();
+
+        $table = new Structure();
+        $table->addHeadCell(new HeadCell($this->lang->t('id'), "id"));
+        $table->addHeadCell(new HeadCell($this->lang->t('user')));
+        $table->addHeadCell(new HeadCell($this->lang->t('service')));
+        $table->addHeadCell(new HeadCell($this->lang->t('mybb_user')));
+        $table->addHeadCell(new HeadCell($this->lang->t('expires')));
+
+        // Wyszukujemy dane ktore spelniaja kryteria
+        $where = '';
+        if (isset($query['search'])) {
+            searchWhere(
+                ["us.id", "us.uid", "u.username", "s.name", "usmeg.mybb_uid"],
+                $query['search'],
+                $where
+            );
+        }
+        // Jezeli jest jakis where, to dodajemy WHERE
+        if (strlen($where)) {
+            $where = "WHERE " . $where . ' ';
+        }
+
+        $result = $this->db->query(
+            "SELECT SQL_CALC_FOUND_ROWS us.id, us.uid, u.username, " .
+                "s.id AS `service_id`, s.name AS `service`, us.expire, usmeg.mybb_uid " .
+                "FROM `" .
+                TABLE_PREFIX .
+                "user_service` AS us " .
+                "INNER JOIN `" .
+                TABLE_PREFIX .
+                $this::USER_SERVICE_TABLE .
+                "` AS usmeg ON usmeg.us_id = us.id " .
+                "LEFT JOIN `" .
+                TABLE_PREFIX .
+                "services` AS s ON s.id = usmeg.service " .
+                "LEFT JOIN `" .
+                TABLE_PREFIX .
+                "users` AS u ON u.uid = us.uid " .
+                $where .
+                "ORDER BY us.id DESC " .
+                "LIMIT " .
+                get_row_limit($pageNumber)
+        );
+
+        $table->setDbRowsAmount($this->db->query('SELECT FOUND_ROWS()')->fetchColumn());
+
+        foreach ($result as $row) {
+            $bodyRow = new BodyRow();
+
+            $bodyRow->setDbId($row['id']);
+            $bodyRow->addCell(
+                new Cell(
+                    $row['uid'] ? $row['username'] . " ({$row['uid']})" : $this->lang->t('none')
+                )
+            );
+            $bodyRow->addCell(new Cell($row['service']));
+            $bodyRow->addCell(new Cell($row['mybb_uid']));
+            $bodyRow->addCell(
+                new Cell(
+                    $row['expire'] == '-1'
+                        ? $this->lang->t('never')
+                        : date($this->settings->getDateFormat(), $row['expire'])
+                )
+            );
+            if (get_privileges("manage_user_services")) {
+                $bodyRow->setDeleteAction(true);
+                $bodyRow->setEditAction(false);
+            }
+
+            $table->addBodyRow($bodyRow);
+        }
+
+        $wrapper->setTable($table);
+
+        return $wrapper;
     }
 
     public function purchaseFormGet(array $query)
