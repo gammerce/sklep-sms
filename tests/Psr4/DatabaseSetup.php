3a86967389a36b165e1c7fa1e8860ccb41250cd2
Michał Budziak <michal.mariusz.b@gmail.com>
2020-02-11 19:14:59 +0100

    Fix income chart

--- tests/Psr4/DatabaseSetup.php
+++ tests/Psr4/DatabaseSetup.php
@@ -34,7 +34,11 @@ class DatabaseSetup
         $this->db->selectDb('sklep_sms');
         $this->db->dropAllTables();
         $this->databaseMigration->setup('abc123', 'admin', 'abc123');
-        $this->db->query("INSERT INTO `ss_payment_platforms` (name, module) VALUES ('cssetti', 'cssetti')");
-        $this->db->query("INSERT INTO `ss_servers` (name, ip, port) VALUES ('amxx', '172.18.0.5', '27015')");
+        $this->db->query(
+            "INSERT INTO `ss_payment_platforms` (name, module) VALUES ('cssetti', 'cssetti')"
+        );
+        $this->db->query(
+            "INSERT INTO `ss_servers` (name, ip, port) VALUES ('amxx', '172.18.0.5', '27015')"
+        );
     }
 }
