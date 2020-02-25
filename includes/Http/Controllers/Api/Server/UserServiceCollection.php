a254ee47f5486957c374bc0c4278f66393311f1d
Michał Budziak <michal.mariusz.b@gmail.com>
2020-02-25 10:20:50 +0100

    Fix finding user services when authorized with token

--- includes/Http/Controllers/Api/Server/UserServiceCollection.php
+++ includes/Http/Controllers/Api/Server/UserServiceCollection.php
@@ -17,9 +17,8 @@ class UserServiceCollection
         $nick = $request->query->get("nick");
         $ip = $request->query->get("ip");
         $steamId = $request->query->get("steam_id");
-        $serverId = $serverAuth->check()
-            ? $serverAuth->server()
-            : $request->query->get("server_id");
+        $server = $serverAuth->server();
+        $serverId = $server ? $server->getId() : $request->query->get("server_id");
 
         $statement = $db->statement(
             <<<EOF
