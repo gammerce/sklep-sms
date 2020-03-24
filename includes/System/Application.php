48a466a59dc441d9467fc5cf963f8750e54cc8c9
Michał Budziak <michal.mariusz.b@gmail.com>
2020-03-24 09:55:03 +0100

    Versaion 3.12.2

--- includes/System/Application.php
+++ includes/System/Application.php
@@ -12,7 +12,7 @@ use Illuminate\Container\Container;
 
 class Application extends Container
 {
-    const VERSION = '3.12.1';
+    const VERSION = '3.12.2';
 
     /** @var array */
     private $providers = [
