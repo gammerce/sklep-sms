c45b2746ea96d74a568188c6e832eb964139a7b0
Michał Budziak <michal.mariusz.b@gmail.com>
2020-04-23 16:31:16 +0200

    Use flex for layout

--- includes/Support/Version.php
+++ includes/Support/Version.php
@@ -16,30 +16,39 @@ class Version
     public function getNewestWeb()
     {
         $response = $this->requester->get(
-            'https://api.github.com/repos/gammerce/sklep-sms/releases/latest'
+            "https://api.github.com/repos/gammerce/sklep-sms/releases/latest",
+            [],
+            [],
+            2
         );
         $content = $response ? $response->json() : null;
 
-        return array_get($content, 'tag_name');
+        return array_get($content, "tag_name");
     }
 
     public function getNewestAmxmodx()
     {
         $response = $this->requester->get(
-            'https://api.github.com/repos/gammerce/plugin-amxmodx/releases/latest'
+            "https://api.github.com/repos/gammerce/plugin-amxmodx/releases/latest",
+            [],
+            [],
+            2
         );
         $content = $response ? $response->json() : null;
 
-        return array_get($content, 'tag_name');
+        return array_get($content, "tag_name");
     }
 
     public function getNewestSourcemod()
     {
         $response = $this->requester->get(
-            'https://api.github.com/repos/gammerce/plugin-sourcemod/releases/latest'
+            "https://api.github.com/repos/gammerce/plugin-sourcemod/releases/latest",
+            [],
+            [],
+            2
         );
         $content = $response ? $response->json() : null;
 
-        return array_get($content, 'tag_name');
+        return array_get($content, "tag_name");
     }
 }
