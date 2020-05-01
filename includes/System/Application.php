6e01759479a728842c5c8debff1893de34e3efe5
Michał Budziak <michal.mariusz.b@gmail.com>
2020-05-01 17:17:32 +0200

    Version 3.14.0-rc.1

--- includes/System/Application.php
+++ includes/System/Application.php
@@ -12,7 +12,7 @@ use Illuminate\Container\Container;
 
 class Application extends Container
 {
-    const VERSION = "3.13.0";
+    const VERSION = "3.14.0-rc.1";
 
     /** @var array */
     private $providers = [
