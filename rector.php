92ebfcbe0693cdb25b49541fb5b71e127eb3db05
Michał Budziak <michal.mariusz.b@gmail.com>
2020-12-08 11:55:36 +0100

    Rector

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\DowngradeSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator) {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+    // paths to refactor; solid alternative to CLI arguments
+    $parameters->set(Option::PATHS, [__DIR__ . '/includes', __DIR__ . '/tests']);
+
+    // Define what rule sets will be applied
+    $parameters->set(Option::SETS, [
+        DowngradeSetList::PHP_80,
+        DowngradeSetList::PHP_74,
+        DowngradeSetList::PHP_73,
+        DowngradeSetList::PHP_72,
+        DowngradeSetList::PHP_71,
+    ]);
+
+    $parameters->set(Option::PHP_VERSION_FEATURES, '5.6');
+};
