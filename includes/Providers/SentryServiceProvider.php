ad714782a154ed2a287c5a80fbb6717fd85ef9fe
Michał Budziak <michal.mariusz.b@gmail.com>
2020-12-12 21:26:37 +0100

    Improve sentry usage

--- includes/Providers/SentryServiceProvider.php
+++ includes/Providers/SentryServiceProvider.php
@@ -4,26 +4,17 @@ namespace App\Providers;
 use App\System\Application;
 use App\System\ExternalConfigProvider;
 use Sentry;
-use Sentry\ClientInterface;
-use Sentry\SentrySdk;
 
 class SentryServiceProvider
 {
-    public function register(Application $app)
+    public function boot(Application $app, ExternalConfigProvider $configProvider)
     {
-        if (!is_testing() && class_exists(ClientInterface::class)) {
-            $app->singleton(ClientInterface::class, function (Application $app) {
-                /** @var ExternalConfigProvider $configProvider */
-                $configProvider = $app->make(ExternalConfigProvider::class);
-
-                Sentry\init([
-                    "dsn" => getenv("SENTRY_DSN") ?: $configProvider->sentryDSN(),
-                    "release" => $app->version(),
-                    "traces_sampler" => 1.0,
-                ]);
-
-                return SentrySdk::getCurrentHub()->getClient();
-            });
+        if (!is_testing()) {
+            Sentry\init([
+                "dsn" => getenv("SENTRY_DSN") ?: $configProvider->sentryDSN(),
+                "release" => $app->version(),
+                "traces_sample_rate" => 1.0,
+            ]);
         }
     }
 }
