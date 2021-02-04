daf3f57c4a6d1d9e19bf3a31eb2fd2779cebb023
Michał Budziak <michal.mariusz.b@gmail.com>
2021-02-04 23:09:59 +0100

    Disable EMULATE_PREPARES

--- includes/Support/Database.php
+++ includes/Support/Database.php
@@ -57,7 +57,7 @@ class Database
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => true,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
         $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
     }
