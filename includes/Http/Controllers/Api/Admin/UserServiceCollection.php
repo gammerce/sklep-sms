9e60a095ad282673be0324b9fc14e8c2864a59bd
Michał Budziak <michal.mariusz.b@gmail.com>
2021-02-06 23:20:32 +0100

    Log user_service_added on upper level

--- includes/Http/Controllers/Api/Admin/UserServiceCollection.php
+++ includes/Http/Controllers/Api/Admin/UserServiceCollection.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Exceptions\InvalidServiceModuleException;
 use App\Http\Responses\ApiResponse;
+use App\Loggers\DatabaseLogger;
 use App\Managers\ServiceModuleManager;
 use App\ServiceModules\Interfaces\IServiceUserServiceAdminAdd;
 use App\Translation\TranslationManager;
@@ -13,6 +14,7 @@ class UserServiceCollection
     public function post(
         $serviceId,
         Request $request,
+        DatabaseLogger $logger,
         ServiceModuleManager $serviceModuleManager,
         TranslationManager $translationManager
     ) {
@@ -23,7 +25,8 @@ class UserServiceCollection
             throw new InvalidServiceModuleException();
         }
 
-        $serviceModule->userServiceAdminAdd($request);
+        $boughtServiceId = $serviceModule->userServiceAdminAdd($request);
+        $logger->logWithActor("log_user_service_added", $boughtServiceId);
 
         return new ApiResponse("ok", $lang->t("service_added_correctly"), true);
     }
