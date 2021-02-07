28fbc0bf5e0e2c57828828485671dd2014f3e2f3
Michał Budziak <michal.mariusz.b@gmail.com>
2021-02-07 22:15:07 +0100

    Version 3.16.2

--- includes/System/Application.php
+++ includes/System/Application.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Application extends Container
 {
-    const VERSION = "3.16.1";
+    const VERSION = "3.16.2";
 
     private array $providers = [
         AppServiceProvider::class,
