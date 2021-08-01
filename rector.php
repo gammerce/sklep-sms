6fe2dc66ccfca34c307c4e74b5a093ab05fa70b6
Michał Budziak <budziam@users.noreply.github.com>
2021-08-01 20:03:15 +0200

    Bump rector (#324)

--- rector.php
+++ rector.php
@@ -1,41 +1,30 @@
 <?php
 
-use Rector\Core\Configuration\Option;
 use Rector\Set\ValueObject\DowngradeSetList;
-use Rector\DowngradePhp70\Rector\FunctionLike\DowngradeTypeReturnDeclarationRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator) {
-    $parameters = $containerConfigurator->parameters();
     $services = $containerConfigurator->services();
 
-    // Define what rule sets will be applied
-    $sets = [];
     $phpVersion = getenv("PHP_VERSION");
     if (version_compare($phpVersion, "8.0") < 0) {
-        $sets[] = DowngradeSetList::PHP_80;
+        $containerConfigurator->import(DowngradeSetList::PHP_80);
     }
     if (version_compare($phpVersion, "7.4") < 0) {
-        $sets[] = DowngradeSetList::PHP_74;
+        $containerConfigurator->import(DowngradeSetList::PHP_74);
     }
     if (version_compare($phpVersion, "7.3") < 0) {
-        $sets[] = DowngradeSetList::PHP_73;
+        $containerConfigurator->import(DowngradeSetList::PHP_73);
     }
     if (version_compare($phpVersion, "7.2") < 0) {
-        $sets[] = DowngradeSetList::PHP_72;
+        $containerConfigurator->import(DowngradeSetList::PHP_72);
+        // It needs to be removed to keep params types in class methods
+        $services->remove(Rector\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector::class);
     }
     if (version_compare($phpVersion, "7.1") < 0) {
-        $sets[] = DowngradeSetList::PHP_71;
+        $containerConfigurator->import(DowngradeSetList::PHP_71);
     }
     if (version_compare($phpVersion, "7.0") < 0) {
-        // It doesn't work correctly with function argument type definition
-        // $sets[] = DowngradeSetList::PHP_70;
-        $services->set(DowngradeTypeReturnDeclarationRector::class);
+        $containerConfigurator->import(DowngradeSetList::PHP_70);
     }
-
-    if (empty($sets)) {
-        exit(0);
-    }
-
-    $parameters->set(Option::SETS, $sets);
 };
