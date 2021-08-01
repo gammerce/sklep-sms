c9d6ca6980c69d1a09110ac37ad7f891e532ebc3
Michał Budziak <michal.mariusz.b@gmail.com>
2021-08-01 19:25:06 +0200

    New API adjustment

--- rector.php
+++ rector.php
@@ -6,36 +6,27 @@ use Rector\DowngradePhp70\Rector\FunctionLike\DowngradeTypeReturnDeclarationRect
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator) {
-    $parameters = $containerConfigurator->parameters();
     $services = $containerConfigurator->services();
 
-    // Define what rule sets will be applied
-    $sets = [];
     $phpVersion = getenv("PHP_VERSION");
     if (version_compare($phpVersion, "8.0") < 0) {
-        $sets[] = DowngradeSetList::PHP_80;
+        $containerConfigurator->import(DowngradeSetList::PHP_80);
     }
     if (version_compare($phpVersion, "7.4") < 0) {
-        $sets[] = DowngradeSetList::PHP_74;
+        $containerConfigurator->import(DowngradeSetList::PHP_74);
     }
     if (version_compare($phpVersion, "7.3") < 0) {
-        $sets[] = DowngradeSetList::PHP_73;
+        $containerConfigurator->import(DowngradeSetList::PHP_73);
     }
     if (version_compare($phpVersion, "7.2") < 0) {
-        $sets[] = DowngradeSetList::PHP_72;
+        $containerConfigurator->import(DowngradeSetList::PHP_72);
     }
     if (version_compare($phpVersion, "7.1") < 0) {
-        $sets[] = DowngradeSetList::PHP_71;
+        $containerConfigurator->import(DowngradeSetList::PHP_71);
     }
     if (version_compare($phpVersion, "7.0") < 0) {
         // It doesn't work correctly with function argument type definition
         // $sets[] = DowngradeSetList::PHP_70;
         $services->set(DowngradeTypeReturnDeclarationRector::class);
     }
-
-    if (empty($sets)) {
-        exit(0);
-    }
-
-    $parameters->set(Option::SETS, $sets);
 };
