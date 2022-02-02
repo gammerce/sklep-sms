b6f9af10875b508425096ac122abe8ad57fcd278
Michał Budziak <budziam@users.noreply.github.com>
2022-02-02 22:24:53 +0100

    Don't include tax when paying with wallet (#353)

---
+++ includes/Payment/Wallet/WalletPriceService.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Payment\Wallet;
+
+use App\Models\Purchase;
+use App\PromoCode\PromoCodeService;
+use App\Support\Money;
+use App\Support\PriceTextService;
+
+class WalletPriceService
+{
+    private PromoCodeService $promoCodeService;
+    private PriceTextService $priceTextService;
+
+    public function __construct(
+        PromoCodeService $promoCodeService,
+        PriceTextService $priceTextService
+    ) {
+        $this->promoCodeService = $promoCodeService;
+        $this->priceTextService = $priceTextService;
+    }
+
+    public function getPrice(Purchase $purchase): ?Money
+    {
+        $price = as_money($purchase->getPayment(Purchase::PAYMENT_PRICE_TRANSFER));
+        if ($price === null) {
+            return null;
+        }
+
+        $promoCode = $purchase->getPromoCode();
+        if ($promoCode) {
+            return $this->promoCodeService->applyDiscount($promoCode, $price);
+        }
+
+        return $price;
+    }
+
+    public function getOldAndNewPrice(Purchase $purchase): array
+    {
+        $price = as_money($purchase->getPayment(Purchase::PAYMENT_PRICE_TRANSFER));
+        $promoCode = $purchase->getPromoCode();
+
+        if ($promoCode) {
+            $discountedPrice = $this->promoCodeService->applyDiscount($promoCode, $price);
+
+            return [
+                "price" => $this->priceTextService->getPriceText($discountedPrice),
+                "old_price" => $price->asPrice(),
+            ];
+        }
+
+        return [
+            "price" => $this->priceTextService->getPriceText($price),
+        ];
+    }
+}
