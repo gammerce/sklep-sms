ae0611dbcd537a5467bf37ab966660b46a616279
Michał Budziak <michal.mariusz.b@gmail.com>
2022-05-21 22:39:37 +0200

    Comply with new interface definition

--- includes/System/Settings.php
+++ includes/System/Settings.php
@@ -41,22 +41,22 @@ class Settings implements ArrayAccess
         return $this->offsetGet($key);
     }
 
-    public function offsetExists($offset)
+    public function offsetExists(mixed $offset): bool
     {
         return isset($this->data[$offset]);
     }
 
-    public function offsetGet($offset)
+    public function offsetGet(mixed $offset): mixed
     {
         return $this->data[$offset];
     }
 
-    public function offsetSet($offset, $value)
+    public function offsetSet(mixed $offset, mixed $value): void
     {
         $this->data[$offset] = $value;
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset(mixed $offset): void
     {
         unset($this->data[$offset]);
     }
