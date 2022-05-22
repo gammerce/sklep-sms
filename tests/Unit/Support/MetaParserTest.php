7e85817e1ae923bb4aa64449e52e3d6cb39e110f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-05-22 19:11:17 +0200

    Add tests for multiple line endings for confidential/.meta

---
+++ tests/Unit/Support/MetaParserTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Unit\Support;
+
+use App\Support\FileSystemContract;
+use App\Support\MetaParser;
+use App\Support\Path;
+use Tests\Psr4\MemoryFileSystem;
+use Tests\Psr4\TestCases\UnitTestCase;
+
+class MetaParserTest extends UnitTestCase
+{
+    private MetaParser $parser;
+    private FileSystemContract $fileSystem;
+    private $path;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fileSystem = new MemoryFileSystem();
+        $this->parser = new MetaParser($this->fileSystem);
+        $this->path = Path::temporary()->to("meta.txt");
+    }
+
+    /** @test */
+    public function shouldParseVersion_withLineFeed()
+    {
+        // given
+        $this->fileSystem->put($this->path, "VERSION=1.1.1" . "\n" . "BUILD=test");
+        // when
+        $meta = $this->parser->parse($this->path);
+        // then
+        $expected = [
+            "VERSION" => "1.1.1",
+            "BUILD" => "test",
+        ];
+        $this->assertSame($expected, $meta);
+    }
+
+    /** @test */
+    public function shouldParseVersion_withCarriageReturn()
+    {
+        // given
+        $this->fileSystem->put($this->path, "VERSION=1.1.1" . "\r" . "BUILD=test");
+        // when
+        $meta = $this->parser->parse($this->path);
+        // then
+        $expected = [
+            "VERSION" => "1.1.1",
+            "BUILD" => "test",
+        ];
+        $this->assertSame($expected, $meta);
+    }
+
+    /** @test */
+    public function shouldParseVersion_withWindowsConvention()
+    {
+        // given
+        $this->fileSystem->put($this->path, "VERSION=1.1.1" . "\r\n" . "BUILD=test");
+        // when
+        $meta = $this->parser->parse($this->path);
+        // then
+        $expected = [
+            "VERSION" => "1.1.1",
+            "BUILD" => "test",
+        ];
+        $this->assertSame($expected, $meta);
+    }
+}
