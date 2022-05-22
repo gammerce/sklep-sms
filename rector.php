8782dc6d65ee5b7b29d70de1fc05e845c033f36f
Michał Budziak <michal.mariusz.b@gmail.com>
2022-05-22 18:06:30 +0200

    Upgrade to RectorConfig

--- rector.php
+++ rector.php
@@ -1,33 +1,34 @@
 <?php
 
 use Rector\Set\ValueObject\DowngradeSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Config\RectorConfig;
 
-return static function (ContainerConfigurator $containerConfigurator) {
-    $services = $containerConfigurator->services();
+return static function (RectorConfig $rectorConfig) {
+    $rectorConfig->parallel();
+    $services = $rectorConfig->services();
 
     $phpVersion = getenv("PHP_VERSION");
     if (version_compare($phpVersion, "8.1") < 0) {
-        $containerConfigurator->import(DowngradeSetList::PHP_81);
+        $rectorConfig->import(DowngradeSetList::PHP_81);
     }
     if (version_compare($phpVersion, "8.0") < 0) {
-        $containerConfigurator->import(DowngradeSetList::PHP_80);
+        $rectorConfig->import(DowngradeSetList::PHP_80);
     }
     if (version_compare($phpVersion, "7.4") < 0) {
-        $containerConfigurator->import(DowngradeSetList::PHP_74);
+        $rectorConfig->import(DowngradeSetList::PHP_74);
     }
     if (version_compare($phpVersion, "7.3") < 0) {
-        $containerConfigurator->import(DowngradeSetList::PHP_73);
+        $rectorConfig->import(DowngradeSetList::PHP_73);
     }
     if (version_compare($phpVersion, "7.2") < 0) {
-        $containerConfigurator->import(DowngradeSetList::PHP_72);
+        $rectorConfig->import(DowngradeSetList::PHP_72);
         // It needs to be removed to keep params types in class methods
         $services->remove(Rector\DowngradePhp72\Rector\ClassMethod\DowngradeParameterTypeWideningRector::class);
     }
     if (version_compare($phpVersion, "7.1") < 0) {
-        $containerConfigurator->import(DowngradeSetList::PHP_71);
+        $rectorConfig->import(DowngradeSetList::PHP_71);
     }
     if (version_compare($phpVersion, "7.0") < 0) {
-        $containerConfigurator->import(DowngradeSetList::PHP_70);
+        $rectorConfig->import(DowngradeSetList::PHP_70);
     }
 };
