38a7d330e065175c4753e1a8d1010924bab0a605
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-05-22 17:59:42 +0200

    Add Path::temporary(), allowing EnvCreatorTest to run on Windows (#387)

--- tests/Unit/Install/EnvCreatorTest.php
+++ tests/Unit/Install/EnvCreatorTest.php
@@ -10,13 +10,15 @@ use Tests\Psr4\TestCases\UnitTestCase;
 class EnvCreatorTest extends UnitTestCase
 {
     private FileSystemContract $fileSystem;
+    private Path $basePath;
     private EnvCreator $envCreator;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->fileSystem = new MemoryFileSystem();
-        $this->envCreator = new EnvCreator(new Path("/test"), $this->fileSystem);
+        $this->basePath = Path::temporary();
+        $this->envCreator = new EnvCreator($this->basePath, $this->fileSystem);
     }
 
     /** @test */
@@ -28,7 +30,7 @@ class EnvCreatorTest extends UnitTestCase
         $this->envCreator->create("example", "80", "my_example", "my_user", "my_password");
 
         // then
-        $envContent = $this->fileSystem->get("/test/confidential/.env");
+        $envContent = $this->fileSystem->get($this->basePath->to("confidential/.env"));
         $this->assertSame(
             <<<EOF
 DB_HOST=example
