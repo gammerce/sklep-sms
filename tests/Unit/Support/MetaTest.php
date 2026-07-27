1ab0eb367019f50b0a05f8972823e83134c2eed2
Michał Budziak <michal.mariusz.b@gmail.com>
2026-07-27 23:17:09 +0200

    Update version to 3.20.0

--- tests/Unit/Support/MetaTest.php
+++ tests/Unit/Support/MetaTest.php
@@ -17,7 +17,7 @@ class MetaTest extends UnitTestCase
     /** @test */
     public function version_is_ok()
     {
-        $this->assertEquals("3.19.2", $this->meta->getVersion());
+        $this->assertEquals("3.20.0", $this->meta->getVersion());
     }
 
     /** @test */
