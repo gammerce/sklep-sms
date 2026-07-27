eefed70b266176c23b35ebf1b697399824e42228
Michał Budziak <budziam@users.noreply.github.com>
2026-07-27 23:02:51 +0200

    fix: add --no-run-if-empty to xargs to prevent rm errors when no files match (#439)

--- remove_unnecessary_files.sh
+++ remove_unnecessary_files.sh
@@ -2,16 +2,16 @@
 
 find vendor -type d | \
 grep -iE "/\.?(demo|doc|docs|example|examples|test|tests|github|circleci|travis)$" | \
-xargs rm -r
+xargs --no-run-if-empty rm -r
 
 find vendor -type f | \
 grep -iE "/\.?(readme|changelog|faq|contributing|history|upgrading|upgrade|package|composer|travis|psalm|phpmd|scrutinizer|coveralls|gush|phpstorm)\.[^/]+$" | \
-xargs rm -r
+xargs --no-run-if-empty rm -r
 
 find vendor -type f | \
 grep -iE "/\.?(php_cs|phpstan|gitignore|gitattributes|editorconfig)[^/]+$" | \
-xargs rm -r
+xargs --no-run-if-empty rm -r
 
 find vendor -type f | \
 grep -iE "/(phpunit\.xml|phpunit\.xml\.dist)$" | \
-xargs rm -r
+xargs --no-run-if-empty rm -r
